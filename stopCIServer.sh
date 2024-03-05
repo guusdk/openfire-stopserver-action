4676edd03f617113e17aed343062ea69ebaf8f3d
Guus der Kinderen <guus.der.kinderen@gmail.com>
2024-03-05 15:25:14 +0100

    Remove unused config options

--- stopCIServer.sh
+++ stopCIServer.sh
@@ -2,29 +2,19 @@
 # Stops an Openfire server (used in Continuous integration testing)
 set -euo pipefail
 
-LOCAL_RUN=false
-
 HOST='example.org'
 
 usage()
 {
-	echo "Usage: $0 [-d] [-l] [-i IPADDRESS] [-h HOST]"
-	echo "    -d: Enable debug mode. Prints commands, and preserves temp directories if used (default: off)"
-	echo "    -l: Launch a local Openfire. (default: off)"
+	echo "Usage: $0 [-i IPADDRESS] [-h HOST]"
 	echo "    -i: Set a hosts file for the given IP and host (or for example.com if running locally). Reverted at exit."
 	echo "    -h: The hostname for the Openfire under test (default: example.org)"
 
 	exit 2
 }
 
-while getopts dlh:i: OPTION "$@"; do
+while getopts h:i: OPTION "$@"; do
 	case $OPTION in
-		d)
-			set -x
-			;;
-		l)
-			LOCAL_RUN=true
-			;;
 		h)
 			HOST="${OPTARG}"
 			;;
@@ -41,7 +31,6 @@ if [[ -n "${IPADDRESS-}" ]]; then
     echo "Resetting hosts file after local running. This may prompt for sudo."
     sudo sed -i'.original' "/$IPADDRESS $HOST/d" /etc/hosts
 fi
-if [[ $LOCAL_RUN == true ]]; then
-    echo "Stopping Openfire"
-    pkill -f openfire.lib #TODO: Can this be made more future-proof against changes in the start script?
-fi
+
+echo "Stopping Openfire"
+pkill -f openfire.lib #TODO: Can this be made more future-proof against changes in the start script?
